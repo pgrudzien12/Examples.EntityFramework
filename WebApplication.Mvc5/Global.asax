20eb4ed0d44c115dd5d36a5a6db0bb6319db620c
Pawel <p.grudzien12@gmail.com>
2015-10-24 13:47:54 +0200

    fixed names and removed some unused view models:

--- WebApplication.Mvc5/Global.asax
+++ WebApplication.Mvc5/Global.asax
@@ -1,1 +1,1 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplication3.MvcApplication" Language="C#" %>
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplication.Mvc5.MvcApplication" Language="C#" %>
--- WebApplication.Mvc5/Properties/AssemblyInfo.cs
+++ WebApplication.Mvc5/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WebApplication3")]
+[assembly: AssemblyTitle("WebApplication.Mvc5")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WebApplication3")]
+[assembly: AssemblyProduct("WebApplication.Mvc5")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
